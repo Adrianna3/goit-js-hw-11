a5f9f81e8c4bd16eae8cc701aba0b54a6870c31c
Adrianna Staniek <adrstani@wp.pl>
2022-07-03 04:40:55 +0200

    done

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,14 +1,21 @@
-
 import axios from 'axios';
 const axios = require('axios');
 
-export function fetchPhoto(name) {
-  return fetch(
-    `https://pixabay.com/api/?key=28305156-2c31ea34b0b957161935cfaa0&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
+
+export const fetchPhoto = async (input, page) => {
+  const key = '28305156-2c31ea34b0b957161935cfaa0';
+  const params = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 40,
+    page: page,
   });
-}
+  if (input.trim() !== '') {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${key}&q=${input}&${params}`
+    );
+    const responseData = response.data;
+    return responseData;
+  }
+};
